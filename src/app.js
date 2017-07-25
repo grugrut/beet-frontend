e6e49d7f4fc6ca0aa25d0de0428e3edd665b7d84
grugrut <grugruglut+github@gmail.com>
2017-07-25 21:52:28 +0900

    Refactoring.

--- src/app.js
+++ src/app.js
@@ -5,47 +5,38 @@ var chart;
 var app = new Vue({
     el: '#app',
     data: {
-        code: '1234'
+        code: '2538'
     },
     methods: {
         graphNow: function() {
-            var req = new XMLHttpRequest();
-            req.onreadystatechange = function() {
-                if (req.readyState == 4 && req.status == 200) {
-                    var rpdata = eval(req.responseText);
-                    var data = google.visualization.arrayToDataTable(rpdata, true);
-                    var options = {
-                        legend:'none',
-                        seriesType: "candlesticks",
-                        series: {1: { type: "line"}}
-                    };
-                    chart.draw(data, options);
-                }
-            };
-            req.open("GET", "http://www.grugrut.net:28080/"+ this.code, false);
-            req.send(null);
-        }        
+            drawChart(this.code);
+        }
     },
     created: function () {
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
-        function drawChart() {
-            var req = new XMLHttpRequest();
-            req.onreadystatechange = function() {
-                if (req.readyState == 4 && req.status == 200) {
-                    var rpdata = eval(req.responseText);
-                    var data = google.visualization.arrayToDataTable(rpdata, true);
-                    var options = {
-                        legend:'none',
-                        seriesType: "candlesticks",
-                        series: {1: { type: "line"}}
-                    };
-                    chart = new google.visualization.ComboChart(document.getElementById('canvas'));
-                    chart.draw(data, options);
-                }
-            };
-            req.open("GET", "http://www.grugrut.net:28080/" , false);
-            req.send(null);
-        }
+        drawChart(this.code);
     }
 });
+
+function drawChart(code) {
+    if (code == undefined) {
+        return;
+    }
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200) {
+            var rpdata = eval(req.responseText);
+            var data = google.visualization.arrayToDataTable(rpdata, true);
+            var options = {
+                legend:'none',
+                seriesType: "candlesticks",
+                series: {1: { type: "line"}}
+            };
+            chart = new google.visualization.ComboChart(document.getElementById('canvas'));
+            chart.draw(data, options);
+        }
+    };
+    req.open("GET", "http://www.grugrut.net:28080/price/"+code, false);
+    req.send(null);
+}
