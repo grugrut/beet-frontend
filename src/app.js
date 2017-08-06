01270945b4936b6869b1981f08659cc050bb2ca0
grugrut <grugruglut+github@gmail.com>
2017-08-06 12:37:55 +0900

    source formatting

--- src/app.js
+++ src/app.js
@@ -17,16 +17,18 @@ var app = new Vue({
             var xhr = new XMLHttpRequest();
             var self = this;
             xhr.open('GET', "http://www.grugrut.net:28080/code/");
-            xhr.onload = function () {
+            xhr.onload = function() {
                 self.codes = eval(xhr.responseText);
                 console.log(self.codes[0].html_url);
             };
             xhr.send();
         }
     },
-    created: function () {
+    created: function() {
         this.fetchCode();
-        google.charts.load('current', {'packages':['corechart']});
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
     }
 });
 
@@ -40,14 +42,18 @@ function drawChart(code) {
             var rpdata = eval(req.responseText);
             var data = google.visualization.arrayToDataTable(rpdata, true);
             var options = {
-                legend:'none',
+                legend: 'none',
                 seriesType: "candlesticks",
-                series: {1: { type: "line"}}
+                series: {
+                    1: {
+                        type: "line"
+                    }
+                }
             };
             chart = new google.visualization.ComboChart(document.getElementById('canvas'));
             chart.draw(data, options);
         }
     };
-    req.open("GET", "http://www.grugrut.net:28080/price/"+code, false);
+    req.open("GET", "http://www.grugrut.net:28080/price/" + code, false);
     req.send(null);
 }
